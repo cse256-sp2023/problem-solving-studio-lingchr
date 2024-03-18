1a89225284940922e35f08db6d6bbee4d697ac8e
lingchr <christine.ling@wustl.edu>
2024-03-17 23:02:21 -0500

    Studio completed

--- view.js
+++ view.js
@@ -1,8 +1,51 @@
 // ---- Define your dialogs  and panels here ----
+var effective_permissions = define_new_effective_permissions("permission", add_icon_col = true, which_permissions = null)
+// show side panel
+$('#sidepanel').append(effective_permissions)
 
+var user_selector = define_new_user_select_field("user", "select", on_user_change = function(selected_user){
+    $('#permission').attr('username', selected_user)
+    $('#permission').attr('filepath', '/C/presentation_documents/important_file.txt')
+    })
+// show user selector/append to side panel element
+$('#sidepanel').append(user_selector)
+    
+// 1. Define the dialog
+var new_dialog = define_new_dialog('new', 'test title')
+
+// 2. Open the dialog on click
+$('.perm_info').click(function(){
+    // stuff that should happen on click goes here
+    console.log('clicked!')
+    new_dialog.dialog('open')
+    console.log($('#permission').attr('filepath'))
+    console.log($('#permission').attr('username'))
+    console.log($(this).attr('permission_name'))
+    
+    var filepath = $('#permission').attr('filepath')
+    var username = $('#permission').attr('username')
+    var permissionType = $(this).attr('permission_name')
+
+    var fileObject = path_to_file[filepath]
+    var userObject = all_users[username]
+   
+    var explanation = allow_user_action(fileObject, userObject, permissionType, explain_why = true)
+    var explanationText = get_explanation_text(explanation)
+    
+    new_dialog.empty().append(explanationText).dialog('open')
+
+})
 
 
 // ---- Display file structure ----
+
+
+
+// define filepath and username attributes
+// $('#permission').attr('filepath', '/C')
+// $('#permission').attr('username', 'administrator')
+
+
 
 // (recursively) makes and returns an html element (wrapped in a jquery object) for a given file object
 function make_file_element(file_obj) {
